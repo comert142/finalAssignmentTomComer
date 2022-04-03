ed0e3dd2d7322b556435d85fb99cec55d2c2aa11
comert142 <102821102+comert142@users.noreply.github.com>
2022-04-03 23:17:43 +0100

    Update BusPathAlgo.java

--- src/main/java/com/mycompany/finalassignmenttomcomer/BusPathAlgo.java
+++ src/main/java/com/mycompany/finalassignmenttomcomer/BusPathAlgo.java
@@ -1,10 +1,22 @@
 
 package com.mycompany.finalassignmenttomcomer;
-
+import java.util.ArrayList;
 /**
  *
  * @author tcome
  */
 public class BusPathAlgo {
-    
+    //this file and class should find the shortest path between two bus stops 
+    //has to be able to access the files and find out information then process them 
+    //going to try and integrate a lot of the code from my last assignment 
+    //graph creation and then an array list of all the bus stops i
+   int maxId = 0;
+   ArrayList<busStop> allbusStops;
+   String fileOfTransfers, timesOfbusStopsFile, justbusStopsFile;//file declaration
+   double[] distanceBetween;
+   
+   
+ 
+       
+
 }
