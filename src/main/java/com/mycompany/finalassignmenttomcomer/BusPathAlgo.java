4fccc2c7a7381afc0ea27b3e930742480df6f9eb
comert142 <102821102+comert142@users.noreply.github.com>
2022-04-04 13:40:39 +0100

    Implementing graphing and edges, cannot find assemble graph method for some reason

--- src/main/java/com/mycompany/finalassignmenttomcomer/BusPathAlgo.java
+++ src/main/java/com/mycompany/finalassignmenttomcomer/BusPathAlgo.java
@@ -24,7 +24,7 @@ import java.util.Scanner;
                this.justbusStopsFile = justbusStopsFile;
                allbusStops = new ArrayList<busStop>(); // this is the array list of all my bus stops 
    }
-   public void findbusStops() {
+   public void findbusStops() throws FileNotFoundException {
                File stops = new File(justbusStopsFile);
                Scanner stopsScanner = new Scanner(stops);
                stopsScanner.nextLine();
@@ -40,7 +40,91 @@ import java.util.Scanner;
                 maxId = Math.max(stopId, maxId);
                 }
                 maxId++;//pursuing scanner through file
-                stopsScanner.close();
+                stopsScanner.close();//now create edges between these indexes in arrays 
+                
+   }//create edges 
+   
+      class Edge {
+
+        int from, to;
+        double cost;
+
+        Edge(int from, int to, double cost) {
+            this.to = to;
+            this.from = from;
+            this.cost = cost;
+        }
+
+      }
+      void createEdges() throws FileNotFoundException{
+                  File stopTimes = new File(timesOfbusStopsFile);
+                  File transfers = new File(fileOfTransfers);
+                  Scanner info;
+       info = new Scanner(transfers);
+                  info.nextLine();
+                          cityBusGraph = new assembleGraph(maxId);//why cant you find my graph???
+                                  String transfer;
+                                  String[] transferValues;
+                                          while (info.hasNextLine()) {
+                                            transfer = info.nextLine();
+                                            transferValues = transfer.split(",");
+                                            int from = Integer.parseInt(transferValues[0]);
+            int to = Integer.parseInt(transferValues[1]);
+            int type = Integer.parseInt(transferValues[2]);
+            int cost = (type == 0) ? 2 : (Integer.parseInt(transferValues[3]) / 100);
+
+            cityBusGraph.addEdge(from, to, cost);
+        }
+      }
+
+
+
+                                  
+
+
+
+
+
+
+
+              
+   class assembleGraph {//would much prefer to put in a multi map TODO THIS LATER !
+               ArrayList<ArrayList<Edge>> adjL = new ArrayList<ArrayList<Edge>>();//EDGE TO BE DEFINED LATER ON SOME METHOD 
+                       assembleGraph(int edges) {
+                                       for (int i = 0; i < edges; i++) {
+                                                        adjL.add(new ArrayList<Edge>());
+                                       }//multiMap INSTEAD WOULD BE BETTER 
+                       }
+                                               ArrayList<Edge> adj(int vertex) {
+                                       return adjL.get(vertex);
+                                               }
+                                               void addEdge(int from, int to, int cost) {
+                                                               adjL.get(from).add(new Edge(from, to, cost));
+                                                               
+                                               }
+   }
+
+
+
+                           
+                       }
+
+//simple iteration
+
+                       }
+
+//array in array initialising 
+               
+
+   
+  
+
+
+                       
+
+
+
+       
    }
        class busStop {//declaring variables and fetching in these next few 
 
